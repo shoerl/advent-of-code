f5b72c07dcd2d950c3e799e08bbdf0e306391df0
shoerl <hoerl.s@northeastern.edu>
2021-12-23 11:44:22 -0500

    day 4 progress

--- day4/day4.py
+++ day4/day4.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-import os 
+import os
+import re
 
-dir_path = os.path.dirname(os.path.realpath(__file__)) + "\\"
+dir_path = os.path.dirname(os.path.realpath(__file__)) + "/"
 file = open(dir_path + "input.txt", "r")
 lines = file.readlines()
 
@@ -13,8 +14,16 @@ class BingoBoard:
             temp_array_real = []
             temp_array_binary = []
             for item in line.split(' '):
-                temp_array_real.push(int(item))
+                temp_array_real.push(int(item.trim()))
                 temp_array_binary.push(0)
             self.board_real.push(temp_array_real)
             self.board_binary.push(temp_array_binary)
+
+order = lines[0]
+lines.remove(0)
+lines.remove(1)
+
+for line in lines
+
+
 
